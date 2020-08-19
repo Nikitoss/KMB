01376f42c2eb5c7288f07454fb8e379beba3f95e
Крутиков Никита Олегович <Krutikov.N.Ol@sberbank.ru>
2020-08-20 01:04:38 +0700

    Черновик первого дня

---
+++ day1/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: ["./src/index.js"],
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+    },
+    // output: {
+    //     filename: "./js/bundle.js",
+    // },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                // exclude: /node_modules/,
+                include: path.resolve(__dirname, 'src/scripts'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    }
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "static/",
+                            useRelativePath: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: "./src/index.html"}),
+        // copy assets to dist
+        new CopyPlugin({
+            patterns: [
+                { from: "./src/assets", to: "./assets" }
+            ],
+        })
+    ]
+};
